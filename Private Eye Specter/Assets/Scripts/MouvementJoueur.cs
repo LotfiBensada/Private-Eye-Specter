f4af9965b89403e30882e76fad71e218a26e8626
biglolo004 <146000255+LotfiBensada@users.noreply.github.com>
2023-12-24 16:55:45 -0500

    setting up character and map + starting mouvement script for character

---
+++ Private Eye Specter/Assets/Scripts/MouvementJoueur.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementJoueur : MonoBehaviour
+{
+
+    [SerializeField] private float niveauForce;  // Le niveau de force à appliquer
+
+    private Rigidbody _rbody; // Le rigidbody où on applique la force
+    private float _vertical;  // La force verticale
+    private float _horizontal; // La force horizontale
+    private Vector3 _positionDepart;
+
+    void Start()
+    {
+        _vertical = 0.0f;
+        _horizontal = 0.0f;
+        _rbody = GetComponent<Rigidbody>();
+        _positionDepart = transform.position;
+    }
+
+    void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 force = new Vector3(_horizontal, 0, _vertical);
+        force *= niveauForce * Time.fixedDeltaTime;
+        _rbody.AddForce(force);
+    }
+
+    public void ReplacerJoueur()
+    {
+        transform.position = _positionDepart;
+        _rbody.velocity = Vector3.zero;
+        _rbody.angularVelocity = Vector3.zero;
+    }
+}
